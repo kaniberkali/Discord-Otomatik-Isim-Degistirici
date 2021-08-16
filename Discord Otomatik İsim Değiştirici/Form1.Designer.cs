856ec190faf6742a44928136b4a467343463481a
Ali Kanıberk <thtramboreis1@gmail.com>
2021-08-16 19:22:53 +0300

    Proje dosyası ekle.

---
+++ Discord Otomatik İsim Değiştirici/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace Discord_Otomatik_İsim_Değiştirici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 38;
+            this.listBox1.Location = new System.Drawing.Point(12, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(603, 232);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(377, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "İsim :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 290);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 45);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(499, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bekletme :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(205, 346);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(98, 45);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "S";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(355, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 45);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Başlat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 42);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 3;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(581, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 42);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mistral", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(420, 41);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Discord Otomatik İsim Değiştirici";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Durum :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 394);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 39);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Bekleniyor.";
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 3;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(525, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 42);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "_";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 3;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(584, 48);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(31, 45);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "T";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(631, 441);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+            this.Name = "Form1";
+            this.Text = "Discord Otomatik İsim Değiştirici";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Discord Otomatik İsim Değiştirici/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using System.IO;
+
+namespace Discord_Otomatik_İsim_Değiştirici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        OpenQA.Selenium.Chrome.ChromeDriver drv; Thread th;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 32)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+            else
+                MessageBox.Show("Çok uzun isim girdiniz", "@kodzamani.tk");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.Text;
+        }
+        private void yükleniyor()
+        {
+            try
+            {
+                string dosya_yolu = @"İsimler.txt";
+                FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+                StreamReader sw = new StreamReader(fs);
+                string yazi = sw.ReadLine();
+                while (yazi != null)
+                {
+                    if (yazi.Length < 32)
+                    {
+                        listBox1.Items.Add(yazi);
+                        label6.Text = yazi + " Eklendi.";
+                    }
+                    else
+                    {
+                        label6.Text = "İsim çok uzun.";
+                    }
+                    yazi = sw.ReadLine();
+                }
+                sw.Close();
+                fs.Close();
+            }
+            catch
+            {
+                label6.Text = "İsimler çekilemedi.";
+            }
+            Thread.Sleep(200);
+
+            label6.Text = "Chrome Ayarları Yapılıyor.";
+            ChromeOptions option = new ChromeOptions();
+            option.AddExcludedArgument("enable-automation");
+            option.AddAdditionalCapability("useAutomationExtension", false);
+            label6.Text = "Chrome Ayarları Yapıldı.";
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            drv = new ChromeDriver(service, option);
+            label6.Text = "Chrome Oluşturuldu.";
+            drv.Navigate().GoToUrl("https://discord.com/login");
+            label6.Text = "Lütfen giriş yapın.";
+            button3.Enabled = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            th = new Thread(yükleniyor);th.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count >= 2)
+            {
+                if (button3.Text == "Başlat")
+                {
+                    check = false;
+                    breaks = false;
+                    kontrol = true;
+                    button3.Text = "Durdur";
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    check = true;
+                    breaks = true;
+                    timer1.Enabled = false;
+                    kontrol = false;
+                    button3.Text = "Başlat";
+                }
+            }
+            else
+                MessageBox.Show("Lütfen en az 2 adet isim girin.", "@kodzamani.tk");
+        }
+        bool kontrol = true;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value == 0)
+                timer1.Interval = 100;
+            else
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value) * 1000;
+            try
+            {
+                if (kontrol == true)
+                    th = new Thread(islemler); th.Start();
+            }
+            catch { }
+        }
+        bool breaks = false;
+        bool check = false;
+        private void islemler()
+        {
+            if (drv.Url.Contains("login") == true)
+                label6.Text = "Giriş Bekleniyor.";
+            else if (drv.Url.Contains("channels/@me") == true)
+                label6.Text = "Sunucu Bekleniyor.";
+            else if (drv.Url.Contains("channels") == true)
+            {
+                label6.Text = "Sunucu Seçildi.";
+                check = true;
+            }
+            if (check == true)
+            {
+                try
+                {
+                    kontrol = false;
+                    for (int i = 0; i <= listBox1.Items.Count; i++)
+                    {
+                        if (breaks == true)
+                        {
+                            kontrol = false;
+                            break;
+                        }
+                        if (i == listBox1.Items.Count)
+                            kontrol = true;
+                        else
+                        {
+                            listBox1.SelectedIndex = i;
+                            label6.Text = "Kullaniciadi Siliniyor.";
+                            drv.FindElement(By.XPath("//html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/nav/div[1]")).Click();
+                            Thread.Sleep(200);
+                            drv.FindElement(By.XPath("//html/body/div/div[6]/div/div/div/div/div[7]/div[1]")).Click();
+                            Thread.Sleep(200);
+                            try
+                            {
+                                drv.FindElement(By.XPath("//html/body/div/div[6]/div[2]/div/div/form/div[2]/button")).Click();
+                            }
+                            catch { }
+                            label6.Text = "Kullaniciadi Silindi.";
+                            Thread.Sleep(200);
+                            drv.FindElement(By.XPath("//html/body/div/div[6]/div[2]/div/div/form/div[2]/div[1]/input")).SendKeys(listBox1.Items[i].ToString());
+                            label6.Text = "Yeni kullaniciadi Yazılıyor.";
+                            Thread.Sleep(200);
+                            drv.FindElement(By.XPath("//html/body/div/div[6]/div[2]/div/div/form/div[3]/button[1]")).Click();
+                            label6.Text = "Kullaniciadi değiştirildi.";
+                        }
+                    }
+                }
+                catch
+                {
+                    kontrol = true;
+                }
+            }
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Move == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - Mouse_X, MousePosition.Y - Mouse_Y);
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Move = 1;
+            Mouse_X = e.X;
+            Mouse_Y = e.Y;
+        }
+        int Move;
+        int Mouse_X;
+        int Mouse_Y;
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Move = 0;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                string dosya_yolu = @"İsimler.txt";
+                FileStream fs = new FileStream(dosya_yolu, FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                    sw.WriteLine(listBox1.Items[i].ToString());
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            catch { }
+            try
+            {
+                if (drv != null)
+                    drv.Quit();
+            }
+            catch { }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
